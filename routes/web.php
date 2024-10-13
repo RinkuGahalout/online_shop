6f178b8e09b62f8bc498cd92af1797c0b4a58e85
ravigahalout837 <ravigahalout@gmail.com>
2024-10-13 15:00:35 +0530

    update code

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\admin\AdminLoginController;
+use App\Http\Controllers\admin\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::group(['prefix' => 'admin'],function(){
+
+    Route::group(['middleware' => 'admin.guest'],function(){
+
+        Route::get('/login',[AdminLoginController::class,'index'])->name('admin.login');
+        Route::post('/authenticate',[AdminLoginController::class,'authenticate'])->name('admin.authenticate');
+    
+    });
+
+    Route::group(['middleware' => 'admin.auth'],function(){
+        Route::get('/dashboard',[DashboardController::class,'index'])->name('admin.dashboard');
+        Route::get('/logout',[DashboardController::class,'logout'])->name('admin.logout');
+    });
+
+
+});
